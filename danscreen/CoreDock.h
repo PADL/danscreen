c6449cc73ad49557b49219047143860974f19de1
Luke Howard <lukeh@padl.com>
2017-10-01 17:22:51 +1100

    initial revision

---
+++ danscreen/CoreDock.h
@@ -0,0 +1,77 @@
+//
+//  CoreDock.h
+//  danscreen
+//
+//  Created by Luke Howard on 01.10.17.
+//  Copyright © 2017 PADL Software Pty Ltd. All rights reserved.
+//
+
+#ifndef CoreDock_h
+#define CoreDock_h
+
+//
+// CoreDockPrivate.h
+// Header file for undocumented Dock SPI
+//
+// Arranged by Tony Arnold
+// Based on CoreDockPrivate.h from http://www.cocoadev.com/index.pl?DockPrefsPrivate
+//
+// Contributors:
+//   Alacatia Labs: Initial version posted at http://www.cocoadev.com/index.pl?DockPrefsPrivate
+//   Tony Arnold: CoreDockGetWorkspacesEnabled, CoreDockSetWorkspacesEnabled, CoreDockSetWorkspacesCount
+//   Steve Voida: CoreDockGetWorkspacesCount
+//
+// Changes:
+//   1.1 - Added attribution for Alacatia labs as originator
+//       - Removed unnecessary reliance on CGSPrivate.h
+//   1.0 - Initial release
+
+typedef enum {
+    kCoreDockOrientationIgnore = 0,
+    kCoreDockOrientationTop = 1,
+    kCoreDockOrientationBottom = 2,
+    kCoreDockOrientationLeft = 3,
+    kCoreDockOrientationRight = 4
+} CoreDockOrientation;
+
+typedef enum {
+    kCoreDockPinningIgnore = 0,
+    kCoreDockPinningStart = 1,
+    kCoreDockPinningMiddle = 2,
+    kCoreDockPinningEnd = 3
+} CoreDockPinning;
+
+typedef enum {
+    kCoreDockEffectGenie = 1,
+    kCoreDockEffectScale = 2,
+    kCoreDockEffectSuck = 3
+} CoreDockEffect;
+
+// Tile size ranges from 0.0 to 1.0.
+extern float CoreDockGetTileSize(void);
+extern void CoreDockSetTileSize(float tileSize);
+
+extern void CoreDockGetOrientationAndPinning(CoreDockOrientation *outOrientation, CoreDockPinning *outPinning);
+// If you only want to set one, use 0 for the other.
+extern void CoreDockSetOrientationAndPinning(CoreDockOrientation orientation, CoreDockPinning pinning);
+
+extern void CoreDockGetEffect(CoreDockEffect *outEffect);
+extern void CoreDockSetEffect(CoreDockEffect effect);
+
+extern Boolean CoreDockGetAutoHideEnabled(void);
+extern void CoreDockSetAutoHideEnabled(Boolean flag);
+
+extern Boolean CoreDockIsMagnificationEnabled(void);
+extern void CoreDockSetMagnificationEnabled(Boolean flag);
+
+// Magnification ranges from 0.0 to 1.0.
+extern float CoreDockGetMagnificationSize(void);
+extern void CoreDockSetMagnificationSize(float newSize);
+
+extern Boolean CoreDockGetWorkspacesEnabled(void);
+extern void CoreDockSetWorkspacesEnabled(Boolean); // This works, but wipes out all of the other spaces prefs. An alternative is to use the ScriptingBridge which works just fine.
+
+extern void CoreDockGetWorkspacesCount(int *rows, int *cols);
+extern void CoreDockSetWorkspacesCount(int rows, int cols);
+
+#endif /* CoreDock_h */
--- danscreen/main.m
+++ danscreen/main.m
@@ -7,11 +7,134 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+
+#import "CoreDock.h"
+
+CGDirectDisplayID gDisplays[3];
+uint32_t gNumDisplays = 0;
+
+static CGError
+SwapDockOrientation(void)
+{
+    CoreDockPinning pinning;
+    CoreDockOrientation oldOrientation, newOrientation;
+    
+    CoreDockGetOrientationAndPinning(&oldOrientation, &pinning);
+    
+    switch (oldOrientation) {
+        case kCoreDockOrientationLeft:
+            newOrientation = kCoreDockOrientationRight;
+            break;
+        case kCoreDockOrientationRight:
+            newOrientation = kCoreDockOrientationLeft;
+            break;
+        default:
+            newOrientation = kCoreDockOrientationIgnore;
+            break;
+    }
+    
+    if (newOrientation != kCoreDockOrientationIgnore) {
+        CoreDockSetOrientationAndPinning(newOrientation, kCoreDockPinningIgnore);
+        printf("Changed dock orientation from %s to %s\n",
+               oldOrientation == kCoreDockOrientationRight ? "right" : "left",
+               newOrientation == kCoreDockOrientationRight ? "right" : "left");
+    }
+    
+    return kCGErrorSuccess;
+}
+
+static CGError
+EnumerateDisplays(void)
+{
+    CGError err;
+    
+    err = CGGetActiveDisplayList(sizeof(gDisplays) / sizeof(gDisplays[0]),
+                                 gDisplays, &gNumDisplays);
+    if (err != kCGErrorSuccess) {
+        fprintf(stderr, "Failed to enumerate displays: %d\n", err);
+        return err;
+    }
+    
+    if (gNumDisplays != 2) {
+        fprintf(stderr, "Maximum of two displays supported\n");
+        return kCGErrorInvalidOperation;
+    }
+    
+    return kCGErrorSuccess;
+}
+
+static CGDirectDisplayID
+GetSecondaryDisplayID(void)
+{
+    CGDirectDisplayID primaryDisplay = CGMainDisplayID();
+    size_t i;
+    
+    for (i = 0; i < gNumDisplays; i++) {
+        if (gDisplays[i] != primaryDisplay)
+            return gDisplays[i];
+    }
+    
+    return 0;
+}
+
+static CGError
+SwapPrimaryDisplays(void)
+{
+    CGError err;
+    CGDisplayConfigRef config = NULL;
+    CGDirectDisplayID primaryDisplay = CGMainDisplayID();
+    CGDirectDisplayID secondaryDisplay = GetSecondaryDisplayID();
+    
+    printf("Primary display ID: %x\n", primaryDisplay);
+    printf("Secondary display ID: %x\n", secondaryDisplay);
+    
+    err = CGBeginDisplayConfiguration(&config);
+    if (err != kCGErrorSuccess) {
+        fprintf(stderr, "Failed to begin display configuration: %d\n", err);
+        return err;
+    }
+    
+    CGRect secondaryBounds = CGDisplayBounds(secondaryDisplay);
+
+    err = CGConfigureDisplayOrigin(config, secondaryDisplay, 0, 0);
+    if (err != kCGErrorSuccess) {
+        fprintf(stderr, "Failed to set primary display origin: %d\n", err);
+        CGCancelDisplayConfiguration(config);
+        return err;
+    }
+    
+    err = CGConfigureDisplayOrigin(config, primaryDisplay,
+                                   -1 * CGRectGetMinX(secondaryBounds),
+                                   -1 * CGRectGetMinY(secondaryBounds));
+    if (err != kCGErrorSuccess) {
+        fprintf(stderr, "Failed to set secondary display origin: %d\n", err);
+        CGCancelDisplayConfiguration(config);
+        return err;
+    }
+
+    err = CGCompleteDisplayConfiguration(config, kCGConfigureForSession);
+    if (err != kCGErrorSuccess) {
+        fprintf(stderr, "Failed to complete display configuration: %d\n", err);
+        return err;
+    }
+    
+    printf("Display %x is now primary display.\n", secondaryDisplay);
+
+    return kCGErrorSuccess;
+}
 
 int main(int argc, const char * argv[]) {
+    CGError err;
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        err = EnumerateDisplays();
+        if (err == kCGErrorSuccess) {
+          err = SwapPrimaryDisplays();
+        }
+        if (err == kCGErrorSuccess) {
+            err = SwapDockOrientation();
+        }
     }
-    return 0;
+    return err;
 }
